24986930dc2c21107857fde41bb8a2d849c66316
Andrew Lorenz <aglorenz777@gmail.com>
2024-06-12 10:49:31 -0700

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello from my app");
+})
+
+app.listen(3000, ()=>{
+    console.log('listening on port ' + chalk.green('3000'));
+});
